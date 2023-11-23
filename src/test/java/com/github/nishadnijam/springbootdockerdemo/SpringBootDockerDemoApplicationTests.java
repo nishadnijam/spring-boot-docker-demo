3fba084f3ac8c566b8ab9e1c855c68c2275d1c0b
nishadnijam <nishad.nijam@gmail.com>
2023-11-23 19:14:30 +0100

    Initial commit

---
+++ src/test/java/com/github/nishadnijam/springbootdockerdemo/SpringBootDockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nishadnijam.springbootdockerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
